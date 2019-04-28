74d943757368ad4fb0f2d8d83a0525d922771daf
Jorge Aguado <48436665+Joragca@users.noreply.github.com>
2019-04-28 16:27:18 +0200

    Create Camino.h

---
+++ Camino.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vector2D.h"
+
+class Camino
+{
+	//Variables:
+	Vector2D posicion;
+
+public:
+
+	//Constructor y destructor
+	Camino();
+	~Camino();
+
+	//Metodos:
+	void getPos(float xi, float yi);
+	
+};
