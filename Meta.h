6c41afac0e741f77a62dd89396fa4892925d0e97
Jorge Aguado <48436665+Joragca@users.noreply.github.com>
2019-04-28 16:23:12 +0200

    Create Meta.h

---
+++ Meta.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"Vector2D.h"
+class Meta
+{
+	//Variables:
+	Vector2D posicion;
+
+
+public:
+	//Constructor y destructor
+	Meta();
+	~Meta();
+
+	//Metodos:
+	void getPos(float xi, float yi);
+};
+
