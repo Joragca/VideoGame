7901e749fd1aff941ea8ea69840bc17815de776e
Jorge Aguado <48436665+Joragca@users.noreply.github.com>
2019-04-28 16:25:09 +0200

    Create GenEnemigos.h

---
+++ GenEnemigos.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include"Vector2D.h"
+
+
+class GenEnemigos
+{
+	//Variables:
+	Vector2D posicion;
+
+	int cuenta;
+
+public:
+
+	//Constructor y destructor
+	GenEnemigos();
+	~GenEnemigos();
+
+	//Metodos:
+
+};
+
