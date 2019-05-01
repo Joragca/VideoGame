29f3b2ca339c16c6a5b582190e420dd738a80fdf
Jorge Aguado <48436665+Joragca@users.noreply.github.com>
2019-05-01 13:56:45 +0200

    Update Mundo.h

--- Mundo.h
+++ Mundo.h
@@ -1,23 +1,39 @@
 #pragma once
+
+#include"Camino.h"
+#include"Disparo.h"
+#include"Enemigo.h"
+#include"GenEnemigos.h"
+#include"Meta.h"
+#include"Mundo.h"
+#include"Torre.h"
+#include"Vector2D.h"
+
 class Mundo
 {
 	//Variables:
-
-	//Declarar aqui todos los objetos:
-
-
-
+	
 	//Coordenadas vista
 	float x_ojo;
 	float y_ojo;
 	float z_ojo;
 
+	//Declarar aqui todos los objetos:
+
+	Torre Torre0;
+
+
+
+	
 public:
 	//Constructor y destructor:
 	Mundo();
 	~Mundo();
 
 	//Metodos:
+	void Dibuja();
+	void Inicializa();
+
 
 };
 
