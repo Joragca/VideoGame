d7e6b59a997eb45ad8a1eb1dbad11668fa42bfec
Jorge Aguado <48436665+Joragca@users.noreply.github.com>
2019-05-01 13:57:39 +0200

    Update Torre.h

--- Torre.h
+++ Torre.h
@@ -1,22 +1,33 @@
 #pragma once
 
+// Esta clase es la base para las demás torres
+//Contendrá atributos y métodos que compartan todas las demás torres
+
+
+
 #include"Vector2D.h"
+
+
 
 class Torre
 {
+protected:
 	//Variables:
 
 	Vector2D posicion;
 	float rango;
 	int nivel;
-	float oro;
+	float oro_necesario;
 
 public:
 	//Constructor y destructor:
-	Torre();
+	//Torre();		//Por defecto
+	Torre(float px, float py);	// Para que al construir tengamos que darle la posicion
+
 	~Torre();
 
 	//Metodos:
 	void getPos(float xi, float yi);
+	void UpgradeTorre(int oro);
 };
 
